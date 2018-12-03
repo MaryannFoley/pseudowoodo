b4b6799322e3d7ed0f1db1096121c076ac10aeb1
Jack Lu <jlu9@stuy.edu>
2018-12-03 13:49:56 -0500

    movie genre text file

--- movie_api.py
+++ movie_api.py
@@ -3,22 +3,33 @@ from urllib import request
 
 from flask import Flask
 
+# title
+# genre
+# id - way to store specific movie for favorites list
+
+
 url = "https://api.themoviedb.org/3/genre/movie/list?api_key=fa2c0e8dd8956b932e67bbe3a99c3255&language=en-US"
 raw = request.urlopen(url)
 info = raw.read()
 genres = json.loads(info)
 # i { 'id' : int, 'name' : 'genre name' }
-# for i in genres["genres"]:
-#     print(i)
+for i in genres["genres"]:
+    print(i)
 
 
 def get_list(genres):
     url_base = "https://api.themoviedb.org/3/discover/movie?api_key=fa2c0e8dd8956b932e67bbe3a99c3255&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
     g_query = "&with_genres="
     for i in genres:
-        g_query += str(i) 
-    print(g_query)
+        g_query += str(i) + "%2C"
+    #print(g_query)
+    url = url_base + g_query
+    raw = request.urlopen(url)
+    info = raw.read()
+    data = json.loads(info)
+    #for i in data['results']:
+    #    print('id : ' + str(i['id']) + ' title : ' + i['title'])
 
-test = [28, 35, 1234]
+test = [28, 35]
 get_list(test)
 
